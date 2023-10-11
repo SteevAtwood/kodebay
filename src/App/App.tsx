f555c313bb641b81148d27ada4064c73fd45a913
Ali <tuc@tuc.com>
2023-10-11 23:55:09 +0300

    add components and files

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,11 @@
 import React from "react";
-
+import Header from "./Components/Header/Header";
 
 export const App = () => {
-  return (<div>Hello World!</div>)
+  return (
+    <div>
+      <Header />
+      <div>Hello World!</div>
+    </div>
+  );
 };
