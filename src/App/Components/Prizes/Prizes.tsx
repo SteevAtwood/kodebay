d45372b2f56633ee38d77024401190a44d05b3db
Yozhef <satwoodt@gmail.com>
2023-10-13 01:40:48 +0200

    some updates

--- src/App/Components/Prizes/Prizes.tsx
+++ src/App/Components/Prizes/Prizes.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import styles from "./Prizes.scss";
+import SVG from "react-inlinesvg";
+import trophy1 from "../../Assets/trophy1.svg";
+import trophy2 from "../../Assets/trophy2.svg";
+import trophy3 from "../../Assets/trophy3.svg";
 
 const Prizes: React.FC = () => {
   return (
@@ -7,22 +11,25 @@ const Prizes: React.FC = () => {
       <h2 className={styles["heading"]}>Призы</h2>
       <div className={styles["center-box"]}>
         <div className={styles["prizeItem"]}>
+          <SVG src={trophy1} className={styles["svg-style"]} />
           1-ое место: 50,000 руб, сертификат участника, мерч Codeby
         </div>
         <div className={styles["prizeItem"]}>
+          <SVG src={trophy2} className={styles["svg-style"]} />
           2-ое место: 40,000 руб, сертификат участника, мерч Codeby
         </div>
         <div className={styles["prizeItem"]}>
+          <SVG src={trophy3} className={styles["svg-style"]} />
           3-е место: 30,000 руб, сертификат участника, мерч Codeby
         </div>
-        <div className={styles["prizeItem"]}>
-          4-10 места: Электронный сертификат участника + статус Codeby Games на
-          форуме
-        </div>
-        <div className={styles.discount}>
+      </div>
+      <div className={styles["otherPrizeItem"]}>
+        4-10 места: Электронный сертификат участника + статус Codeby Games на
+        форуме
+        <p className={styles.discount}>
           Скидки на курсы от Академии Кодебай для первой тройки победителей:
           50%, 40%, 30% соответственно.
-        </div>
+        </p>
       </div>
     </div>
   );
