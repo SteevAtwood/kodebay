1f9e43ace8a4bde0089a5792e9cca95a44f11715
Ali <tuc@tuc.com>
2023-10-12 23:07:00 +0300

    new icons on contacts

--- src/App/Components/ContactInfo/ContactInfo.tsx
+++ src/App/Components/ContactInfo/ContactInfo.tsx
@@ -1,14 +1,24 @@
 import React from "react";
 import styles from "./ContactInfo.scss";
+import SVG from "react-inlinesvg";
+import telegramIcon from "../../Assets/telegram.svg";
+import discordIcon from "../../Assets/discord.svg";
+import forumIcon from "../../Assets/codeby_link.svg";
 
 const ContactInfo: React.FC = () => {
   return (
     <div className={styles["container"]}>
       <h2 className={styles["heading"]}>Как и где искать команду</h2>
       <div className={styles["center-box"]}>
-        <p>Телеграм</p>
-        <p>Дискорд</p>
-        <p>Форум Codeby Games</p>
+        <div className={styles["icon-box"]}>
+          <SVG src={telegramIcon} className={styles["svg-image"]} />
+        </div>
+        <div className={styles["icon-box"]}>
+          <SVG src={discordIcon} className={styles["svg-image"]} />
+        </div>
+        <div className={styles["icon-box"]}>
+          <SVG src={forumIcon} className={styles["svg-image"]} />
+        </div>
       </div>
     </div>
   );
