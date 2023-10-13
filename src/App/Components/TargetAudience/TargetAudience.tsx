63c1b3f38a604ec3578e098795b074ebb840c83f
Ali <tuc@tuc.com>
2023-10-13 06:20:54 +0300

    adding icons to the 'Target audience' category

--- src/App/Components/TargetAudience/TargetAudience.tsx
+++ src/App/Components/TargetAudience/TargetAudience.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import styles from "./TargetAudience.scss";
+import SVG from "react-inlinesvg";
+import student from "../../Assets/student.svg";
+import programmer from "../../Assets/programmer.svg";
+import hacker from "../../Assets/hacker.svg";
+import team from "../../Assets/team.svg";
 
 const TargetAudience: React.FC = () => {
   return (
@@ -7,11 +12,23 @@ const TargetAudience: React.FC = () => {
       <h2 className={styles["heading"]}>Для кого</h2>
       <div className={styles["center-box"]}>
         <div className={styles["details"]}>
-          <ul>
-            <li>- Студенты и обучающиеся</li>
-            <li>- IT-специалисты и разработчики</li>
-            <li>- Специалисты по информационной безопасности</li>
-            <li>- Корпоративные команды безопасности</li>
+          <ul className={styles["target-audience-list"]}>
+            <li>
+              <SVG src={student} className={styles["icon"]} /> Студенты и
+              обучающиеся
+            </li>
+            <li>
+              <SVG src={programmer} className={styles["icon"]} /> IT-специалисты
+              и разработчики
+            </li>
+            <li>
+              <SVG src={hacker} className={styles["icon"]} /> Специалисты по
+              информационной безопасности
+            </li>
+            <li>
+              <SVG src={team} className={styles["icon"]} /> Корпоративные
+              команды безопасности
+            </li>
           </ul>
         </div>
       </div>
